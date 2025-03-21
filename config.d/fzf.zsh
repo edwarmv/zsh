9106bb7072c1ccbb5a8346d96787eed08855dc99
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-03-21 13:20:54 -0400

    update fzf theme

--- config.d/fzf.zsh
+++ config.d/fzf.zsh
@@ -1,54 +1,26 @@
-FZF_DEFAULT_OPTS="
---bind=alt-j:preview-down,alt-k:preview-up,alt-d:preview-page-down,alt-u:preview-page-up,change:first,alt-a:select-all,alt-A:deselect-all,alt-p:toggle-preview
---preview-window noborder
---layout=reverse
-"
-# export FZF_DEFAULT_OPTS="
-# --bind=alt-j:preview-down,alt-k:preview-up,alt-d:preview-page-down,alt-u:preview-page-up,change:first,alt-a:select-all,alt-A:deselect-all,alt-p:toggle-preview
-# --preview-window noborder
-# --layout=reverse
-# --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796
-# --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6
-# --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#chttps://medium.com/codex/how-and-why-you-should-split-your-bashrc-or-zshrc-files-285e5cc3c8436a0f6,hl+:#ed8796
-# --color=separator:#8aadf4
-# --color=border:#8aadf4
-# "
-
-# gruvbox_material
-# export FZF_DEFAULT_OPTS="
-# --bind=alt-j:preview-down,alt-k:preview-up,alt-d:preview-page-down,alt-u:preview-page-up,change:first,alt-a:select-all,alt-A:deselect-all,alt-p:toggle-preview
-# --preview-window noborder
-# --layout=reverse
-# --color=fg:#928374,bg:-1,hl:#7daea3
-# --color=fg+:#928374,bg+:#3c3836,hl+:#7daea3
-# --color=info:#d8a657,prompt:#d3869b,pointer:#d3869b
-# --color=marker:#a9b665,spinner:#d3869b,header:#a9b665,preview-bg:-1
-# --color=border:#928374
-# --color=separator:#665c54
-# "
-
-# tokyonight_storm
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+export FZF_DEFAULT_OPTS="
+  --bind=alt-j:preview-down,alt-k:preview-up,alt-d:preview-page-down,alt-u:preview-page-up,change:first,alt-a:select-all,alt-A:deselect-all,alt-p:toggle-preview \
+  --preview-window=noborder \
+  --layout=reverse \
   --highlight-line \
   --info=inline-right \
   --ansi \
-  --layout=reverse \
   --border=none
-  --color=bg+:#363d59 \
-  --color=bg:#1f2335 \
-  --color=border:#29a4bd \
-  --color=fg:#565f89 \
-  --color=fg+:#565f89 \
-  --color=gutter:#1f2335 \
-  --color=header:#ff9e64 \
-  --color=hl+:#2ac3de \
-  --color=hl:#2ac3de \
-  --color=info:#545c7e \
-  --color=marker:#ff007c \
-  --color=pointer:#ff007c \
-  --color=prompt:#2ac3de \
-  --color=query:#c0caf5:regular \
-  --color=scrollbar:#29a4bd \
-  --color=separator:#ff9e64 \
-  --color=spinner:#ff007c \
+  --color=bg+:8 \
+  --color=bg:0 \
+  --color=border:8 \
+  --color=fg:7 \
+  --color=fg+:15 \
+  --color=gutter:0 \
+  --color=header:5 \
+  --color=hl+:6 \
+  --color=hl:5 \
+  --color=info:12 \
+  --color=marker:2 \
+  --color=pointer:4 \
+  --color=prompt:3 \
+  --color=query:7:regular \
+  --color=scrollbar:4 \
+  --color=separator:8 \
+  --color=spinner:1 \
 "
